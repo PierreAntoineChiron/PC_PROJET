97e44f57ce339cb68e4d4d27c88018cfd5424770
Gaelle Leroux <gaelleleroux974@gmail.com>
2022-06-11 18:55:38 +0200

    commentaire sur faire calcul

--- _faites_calcul.py
+++ _faites_calcul.py
@@ -6,12 +6,12 @@ def fils_calculette(queue):
 
     while True:
         global res
-        commande = queue.get()
+        commande = queue.get() # reçoit le calcul à faire
         print("Le fils à recu ", commande)
-        res=eval(commande)
+        res=eval(commande) # calcul la réponse 
 
         print("Dans fils, le résultat =", res)
-        queue.put(res)
+        queue.put(res) # envoie la réponse du calculs
 
         print("Le fils a envoyé", res)
         time.sleep(1)
@@ -25,7 +25,7 @@ if __name__ == "__main__" :
 
     queue = mp.Queue()
     for i in range(N):
-        calculateur = mp.Process(target=fils_calculette, args=(queue,))
+        calculateur = mp.Process(target=fils_calculette, args=(queue,)) # création du calculateur
         calculateur.start()
         time.sleep(0.5)
 
@@ -34,11 +34,11 @@ if __name__ == "__main__" :
         opd1 = random.randint(1,10)
         opd2 = random.randint(1,10)
         operateur=random.choice(['+', '-', '*', '/','**','%','//'])
-        str_commande = str(opd1) + operateur + str(opd2)
+        str_commande = str(opd1) + operateur + str(opd2) # création du calcul demandé
 
-        queue.put(str_commande)
-        print("Le père va demander à faire : ", str_commande)
-        reponse = queue.get()
-        print("Le Pere a recu ", reponse)
+        queue.put(str_commande) # envoie du calcul
+        print("Le père va demander à faire : ", str_commande) # affichage
+        reponse = queue.get() # réponse du calcul
+        print("Le Pere a recu ", reponse) # affichage
         print('-'* 60)
         time.sleep(1)
--- _faites_calcul2.py
+++ _faites_calcul2.py
@@ -5,16 +5,16 @@ import sys,os,time,random
 def fils_calculette(queue,o):
 
     a_envoyer = []
-    while fin.value !=0 :
-        commande = queue.get()
-        res=eval(commande[:-1])
+    while fin.value !=0 : # boucle tant que toutes les calculs ne sont pas récupérée
+        commande = queue.get() # récupère le calcul à faire
+        res=eval(commande[:-1]) # résout le calcul en enlevant l'identifiant
         lock_fin.acquire()
         fin.value-=1
         lock_fin.release()
-        res = str(res) + str(commande[len(commande)-1])
-        a_envoyer.append(res)
+        res = str(res) + str(commande[len(commande)-1]) # rajoute l'identifiant à la réponse
+        a_envoyer.append(res) #stocke dans la liste a_envoyer
     
-    for i in range(len(a_envoyer)):
+    for i in range(len(a_envoyer)): # envoie toutes le réponses qu'il a calculer 
         queue.put(a_envoyer[i])
     cf_sema.acquire()
     cf.value+=1
@@ -29,27 +29,27 @@ def pere_demandeur(queue,o):
     opd1 = random.randint(1,10)
     opd2 = random.randint(1,10)
     operateur=random.choice(['+', '-', '*', '/','**','%','//'])
-    str_commande = str(opd1) + operateur + str(opd2)
+    str_commande = str(opd1) + operateur + str(opd2) # création du calcul demandé
     print("Le père "+str(o) +  " va demander à faire : ", str_commande)
-    str_commande = str_commande + str(o)
-    queue.put(str_commande)
+    str_commande = str_commande + str(o) # ajout de l'identifiant du père
+    queue.put(str_commande) # envoie de la commande
     lock_fin.acquire()
-    fin.value+=1
+    fin.value+=1 # incrémentation de fin
     lock_fin.release()
     test = 0
     x=0
-    while x != 3 :
+    while x != 3 : # on attend que toutes les réponses aient été envoyé dans la file
         cf_sema.acquire()
         x=cf.value
         cf_sema.release()
         time.sleep(1)
         pass
-    while test == 0 :
+    while test == 0 : # attend que le père trouve sa réponse
         reponse=queue.get()
-        if int(reponse[len(reponse)-1])==o :
+        if int(reponse[len(reponse)-1])==o : # vérifie  l'identifant de la réponse qu'il a récupérée
             test = 1
         else :
-            queue.put(reponse)
+            queue.put(reponse) # renvoie la réponse si ce n'est pas la sienne
         time.sleep(1)
     print("Le Pere  " + str(o) + " a recu ", reponse[:-1])
 
@@ -75,7 +75,7 @@ if __name__ == "__main__" :
     pere_demandeur3.start()
 
     x=0
-    while x !=3 :
+    while x !=3 : # attend que tous les calculs aient été envoyé
         lock_fin.acquire()
         x=fin.value
         lock_fin.release()
